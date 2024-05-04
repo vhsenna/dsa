619a35359119dfdaa1c645ac953c446120899ea2
Victor Senna <vhsenna@gmail.com>
2024-05-04 18:45:15 -0300

    LeetCode 1480

---
+++ leetcode/1480/main.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def runningSum(nums: List[int]) -> List[int]:
+    for i in range(1, len(nums)):
+        nums[i] = nums[i - 1] + nums[i]
+
+    return nums
+
+
+print(runningSum([1, 2, 3, 4]))
+print(runningSum([1, 1, 1, 1, 1]))
+print(runningSum([3, 1, 2, 10, 1]))
