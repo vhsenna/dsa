d6bb1f5d7bdfa2fa46416dd5f30d08798893d2c9
Victor Senna <vhsenna@gmail.com>
2024-05-21 12:22:03 -0300

    Chapter 04

--- grokking-algorithms/chapter04/ex04.py
+++ grokking-algorithms/chapter04/ex04.py
@@ -1,21 +1,7 @@
 # Remember binary search from chapter 1? It's a D&C algorithm, too. Can you come up with the base case and recursive
 # case for binary search?
 
-def quicksort(array):
-    if len(array) < 2:
-        return array
-    else:
-        pivot = array[0]
-        less = []
-        greater = []
-
-        for i in array[1:]:
-            if i <= pivot:
-                less.append(i)
-        for i in array[1:]:
-            if i > pivot:
-                greater.append(i)
-        return quicksort(less) + [pivot] + quicksort(greater)
-
-
-print(quicksort([10, 5, 2, 3]))
+# The base case for binary search is an array with one item.
+# If the item you're looking for matches the item in the array, you found it! Otherwise, it isn't in the array.
+# In the recursive case for binary search, you split the array in half, throw away one half, and call binary search on
+# the other half.
